f29b127c15187105e50c0cdaec8ccd1d57f158a0
Jacob D. Parr <me@jacobparr.com>
2022-04-14 10:36:23 -0500

    Update tokens.py

--- dbacademy/dbrest/tokens.py
+++ dbacademy/dbrest/tokens.py
@@ -6,8 +6,21 @@ class TokensClient:
         self.client = client
         self.token = token
         self.endpoint = endpoint
-        self.base_url = f"{self.endpoint}/api/2.0/token/list"
+        self.base_url = f"{self.endpoint}/api/2.0/token"
 
     def list(self):
-        results = self.client.execute_get_json(f"{self.base_url}")
+        results = self.client.execute_get_json(f"{self.base_url}/list")
         return results["token_infos"]
+
+    def create(self, comment: str, lifetime_seconds: int):
+        params = {
+            "comment": comment,
+            "lifetime_seconds": lifetime_seconds
+        }
+        return self.client.execute_post_json(f"{self.base_url}/create", params)
+
+    def revoke(self, token_id):
+        params = {
+            "token_id": token_id
+        }
+        return self.client.execute_post_json(f"{self.base_url}/delete", params)
