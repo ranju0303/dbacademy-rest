09cd394fd94dd1b7328db8052bfa83e04f9d8756
Jacob D. Parr <me@jacobparr.com>
2022-04-14 11:02:57 -0500

    Removed support for "local" config and for initializing the client with a token and/or endpoint

--- dbacademy/dbrest/__init__.py
+++ dbacademy/dbrest/__init__.py
@@ -1,17 +1,16 @@
-# Databricks notebook source
-import configparser
-import os
+import typing
+
 
 class DBAcademyRestClient:
   
-    def __init__(self, local=False, config_file=None, profile="DEFAULT", throttle=0, endpoint=None):
+    def __init__(self, endpoint: str = None, token: str = None, throttle: int = 0):
         import requests
         from urllib3.util.retry import Retry
         from requests.adapters import HTTPAdapter
 
         self.throttle = throttle
 
-        self.read_timeout = 300 # seconds
+        self.read_timeout = 300   # seconds
         self.connect_timeout = 5  # seconds
 
         backoff_factor = self.connect_timeout
@@ -20,17 +19,18 @@ class DBAcademyRestClient:
         self.session = requests.Session()
         self.session.mount('https://', HTTPAdapter(max_retries=retry))
 
-        if not local:
-            from dbacademy import dbgems
-
-            self.token = dbgems.get_notebooks_api_token()
-            if endpoint:
-                self.endpoint = endpoint
-            else:
-                self.endpoint = dbgems.get_notebooks_api_endpoint()
+        if self.endpoint is not None:
+            self.endpoint = endpoint
         else:
-            self._get_local_credentials(config_file, profile)
-        
+            from dbacademy import dbgems
+            self.endpoint = dbgems.get_notebooks_api_endpoint()
+
+        if self.token is not None:
+            self.token = token
+        else:
+            from dbacademy import dbgems
+            self.token = dbgems.get_notebooks_api_token()
+
         if self.throttle > 0:
             s = "" if self.throttle == 1 else "s"
             print(f"** WARNING ** Requests are being throttled by {self.throttle} second{s} per request.")
@@ -42,10 +42,6 @@ class DBAcademyRestClient:
 
     def throttle_calls(self):
         import time
-        # if self.throttle > 0:
-        #     s = "" if self.throttle == 1 else "s"
-        #     print(f"** Throttling requests by {self.throttle} second{s} per request.")
-
         time.sleep(self.throttle)
 
     def clusters(self):
@@ -97,16 +93,6 @@ class DBAcademyRestClient:
 
         return WorkspaceClient(self, self.token, self.endpoint)
 
-    def _get_local_credentials(self, config_file, profile):
-        if config_file is None:
-            config_file = os.environ["HOME"] + "/.databrickscfg"
-        config = configparser.ConfigParser()
-        config.read(config_file)
-
-        self.endpoint = config.get(profile, "host")
-        self.username = config.get(profile, "username")
-        self.token = config.get(profile, "token")
-
     def execute_patch_json(self, url: str, params: dict, expected=200) -> dict:
         return self.execute_patch(url, params, expected).json()
 
@@ -146,12 +132,12 @@ class DBAcademyRestClient:
         self.throttle_calls()
         return response
 
-    def execute_get_json(self, url: str, expected=200) -> dict:
+    def execute_get_json(self, url: str, expected=200) -> typing.Union[dict, None]:
         response = self.execute_get(url, expected)
 
         if response.status_code == 200:
             return response.json()
-        else: # For example, expected includes 404
+        else:  # For example, expected includes 404
             return None
 
     def execute_get(self, url: str, expected=200):
@@ -163,11 +149,11 @@ class DBAcademyRestClient:
         self.throttle_calls()
         return response
 
-    def execute_delete_json(self, url: str, expected=[200,404]) -> dict:
+    def execute_delete_json(self, url: str, expected=[200, 404]) -> dict:
         response = self.execute_delete(url, expected)
         return response.json()
 
-    def execute_delete(self, url: str, expected=[200,404]):
+    def execute_delete(self, url: str, expected=[200, 404]):
         expected = self.expected_to_list(expected)
 
         response = self.session.delete(url, headers={"Authorization": f"Bearer {self.token}"}, timeout=(self.connect_timeout, self.read_timeout))
