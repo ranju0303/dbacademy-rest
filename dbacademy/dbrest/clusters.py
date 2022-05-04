c0fa2c97b8b09dbcf81a21273a68420ad9cc16a9
Jacob D. Parr <me@jacobparr.com>
2022-05-04 17:15:42 -0500

    Added cluster convenience methods

--- dbacademy/dbrest/clusters.py
+++ dbacademy/dbrest/clusters.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
+from dbacademy import dbgems
 from dbacademy.dbrest import DBAcademyRestClient
 
 
@@ -10,3 +11,18 @@ class ClustersClient:
 
     def get(self, cluster_id):
         return self.client.execute_get_json(f"{self.endpoint}/api/2.0/clusters/get?cluster_id={cluster_id}")
+
+    def get_current_spark_version(self):
+        from dbacademy import dbgems
+        cluster_id = dbgems.get_tags()["clusterId"]
+        return self.get(cluster_id).get("spark_version", None)
+
+    def get_current_instance_pool_id(self):
+        from dbacademy import dbgems
+        cluster_id = dbgems.get_tags()["clusterId"]
+        return self.get(cluster_id).get("spark_version", None)
+
+    def get_current_node_type_id(self):
+        from dbacademy import dbgems
+        cluster_id = dbgems.get_tags()["clusterId"]
+        return self.get(cluster_id).get("spark_version", None)
