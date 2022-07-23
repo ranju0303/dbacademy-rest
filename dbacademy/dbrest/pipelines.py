413fcc7226b672129e51513a95194048dea9043f
Jacob D. Parr <jacob.parr@databricks.com>
2022-07-22 21:24:59 -0500

    Update pipelines.py

--- dbacademy/dbrest/pipelines.py
+++ dbacademy/dbrest/pipelines.py
@@ -89,8 +89,7 @@ class PipelinesClient:
             self.update_from_dict(pipeline_id, params)
             return pipeline_id
         else:
-            return self.create_from_dict(params)["pipeline_id"]
-
+            return self.create_from_dict(params).get("pipeline_id")
 
     def update(self, pipeline_id: str, name: str, storage: str, target: str, continuous: bool = False, development: bool = True, configuration: dict = None, notebooks: list = None, libraries: list = None, clusters: list = None, min_workers: int = 0, max_workers: int = 0, photon: bool = True):
         params = self.to_dict(name=name,
