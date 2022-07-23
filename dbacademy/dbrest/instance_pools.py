f5745c9b5fc7c792583ad1f7dfa56a595c23a566
Jacob D. Parr <jacob.parr@databricks.com>
2022-07-23 15:52:47 -0500

    Update instance_pools.py

--- dbacademy/dbrest/instance_pools.py
+++ dbacademy/dbrest/instance_pools.py
@@ -33,25 +33,42 @@ class InstancePoolsClient:
 
         return self.client.execute_post_json(f"{self.base_uri}/create", params=definition)
 
-    def update_by_name(self, name: str, definition: dict):
-        pool = self.get_by_name(name)
-        assert pool is not None, f"A pool named \"{name}\" was not found."
+    def update_by_name(self, instance_pool_name: str, min_idle_instances: int = None, max_capacity: int = None, idle_instance_autotermination_minutes: int = None):
+
+        pool = self.get_by_name(instance_pool_name)
+        assert pool is not None, f"A pool named \"{instance_pool_name}\" was not found."
 
         instance_pool_id = pool.get("instance_pool_id")
 
-        return self.update_by_id(instance_pool_id, name, definition)
+        return self.update_by_id(instance_pool_id=instance_pool_id,
+                                 instance_pool_name=instance_pool_name,
+                                 min_idle_instances=min_idle_instances,
+                                 max_capacity=max_capacity,
+                                 idle_instance_autotermination_minutes=idle_instance_autotermination_minutes)
 
-    def update_by_id(self, instance_pool_id: str, name: str, definition: dict):
-        import json
+    def update_by_id(self, instance_pool_id: str, instance_pool_name: str, min_idle_instances: int = None, max_capacity: int = None, idle_instance_autotermination_minutes: int = None):
         assert type(instance_pool_id) == str, f"Expected id to be of type str, found {type(instance_pool_id)}"
-        assert type(name) == str, f"Expected name to be of type str, found {type(name)}"
-        assert type(definition) == dict, f"Expected definition to be of type dict, found {type(definition)}"
+        assert type(instance_pool_name) == str, f"Expected name to be of type str, found {type(instance_pool_name)}"
+        assert type(min_idle_instances) == int, f"Expected min_idle_instances to be of type int, found {type(min_idle_instances)}"
+        assert type(max_capacity) == int, f"Expected max_capacity to be of type int, found {type(max_capacity)}"
+        assert type(idle_instance_autotermination_minutes) == int, f"Expected idle_instance_autotermination_minutes to be of type int, found {type(idle_instance_autotermination_minutes)}"
+
+        pool = self.get_by_id(instance_pool_id)
+
+        instance_pool_name = pool.get("instance_pool_name") if instance_pool_name is None else instance_pool_name
+        min_idle_instances = pool.get("min_idle_instances") if min_idle_instances is None else min_idle_instances
+        max_capacity = pool.get("max_capacity") if max_capacity is None else max_capacity
+        idle_instance_autotermination_minutes = pool.get("idle_instance_autotermination_minutes") if idle_instance_autotermination_minutes is None else idle_instance_autotermination_minutes
 
         params = {
             "instance_pool_id": instance_pool_id,
-            "instance_pool_name": name,
-            "definition": json.dumps(definition)
+            "instance_pool_name": instance_pool_name,
         }
+
+        if max_capacity is not None: params["max_capacity"] = max_capacity
+        if min_idle_instances is not None: params["min_idle_instances"] = min_idle_instances
+        if idle_instance_autotermination_minutes is not None: params["idle_instance_autotermination_minutes"] = idle_instance_autotermination_minutes
+
         return self.client.execute_post_json(f"{self.base_uri}/edit", params=params)
 
     def create_or_update(self, name, definition):
