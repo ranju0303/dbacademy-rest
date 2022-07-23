b1e7a40244314917c53669f22b876f558e655b24
Jacob D. Parr <jacob.parr@databricks.com>
2022-07-23 15:00:51 -0500

    Update cluster_policies.py

--- dbacademy/dbrest/cluster_policies.py
+++ dbacademy/dbrest/cluster_policies.py
@@ -57,8 +57,14 @@ class ClusterPolicyClient:
         }
         return self.client.execute_post_json(f"{self.base_uri}/edit", params=params)
 
-    def create_or_update(self):
-        pass
+    def create_or_update(self, name, definition):
+        policy = self.get_by_name(name)
+
+        if policy is None:
+            self.create(name, definition)
+        else:
+            policy_id = policy.get("policy_id")
+            self.update_by_id(policy_id, name, definition)
 
     def delete_by_id(self, policy_id):
         return self.client.execute_post_json(f"{self.base_uri}/delete", params={"policy_id": policy_id})
@@ -69,24 +75,3 @@ class ClusterPolicyClient:
 
         policy_id = policy.get("policy_id")
         return self.delete_by_id(policy_id)
-
-    # def list(self):
-    #     return self.client.execute_get_json(f"{self.client.endpoint}/api/2.0/clusters/list")
-    #
-    # def get(self, cluster_id):
-    #     return self.client.execute_get_json(f"{self.client.endpoint}/api/2.0/clusters/get?cluster_id={cluster_id}")
-    #
-    # def get_current_spark_version(self):
-    #     from dbacademy import dbgems
-    #     cluster_id = dbgems.get_tags()["clusterId"]
-    #     return self.get(cluster_id).get("spark_version", None)
-    #
-    # def get_current_instance_pool_id(self):
-    #     from dbacademy import dbgems
-    #     cluster_id = dbgems.get_tags()["clusterId"]
-    #     return self.get(cluster_id).get("instance_pool_id", None)
-    #
-    # def get_current_node_type_id(self):
-    #     from dbacademy import dbgems
-    #     cluster_id = dbgems.get_tags()["clusterId"]
-    #     return self.get(cluster_id).get("node_type_id", None)
