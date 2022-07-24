6d6711762e0e54b6964f398c92bd7ade483a5e82
Jacob D. Parr <jacob.parr@databricks.com>
2022-07-24 00:09:37 -0500

    Update endpoints.py

--- dbacademy/dbrest/sql/endpoints.py
+++ dbacademy/dbrest/sql/endpoints.py
@@ -67,7 +67,6 @@ class SqlEndpointsClient:
         return None
 
     def list(self):
-        print(self.base_uri)
         result = self.client.execute_get_json(self.base_uri)
         return result.get("warehouses", [])
 
