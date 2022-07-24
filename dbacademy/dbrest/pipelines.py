a58dc252cdc76e2470fea59bedf7ad3ef57ddc38
Jacob D. Parr <jacob.parr@databricks.com>
2022-07-23 20:09:21 -0500

    Update pipelines.py

--- dbacademy/dbrest/pipelines.py
+++ dbacademy/dbrest/pipelines.py
@@ -75,17 +75,10 @@ class PipelinesClient:
         return self.client.execute_post_json(f"{self.base_uri}", params)
 
     def create_or_update(self, name: str, storage: str, target: str, continuous: bool = False, development: bool = True, configuration: dict = None, notebooks: list = None, libraries: list = None, clusters: list = None, min_workers: int = 0, max_workers: int = 0, photon: bool = True, pipeline_id=None):
-        import json
-        print(f"Create or Update: {libraries}")
 
         if pipeline_id is not None:
             pipeline = self.get_by_id(pipeline_id)
-            print("-"*80)
-            json.dumps(pipeline, indent=4)
-            print("-"*80)
-            if libraries is None: libraries = pipeline.get("libraries")
-
-        print(f"Create or Update: {libraries}")
+            if notebooks is None: notebooks = pipeline.get("notebooks")
 
         params = self.to_dict(name=name,
                               storage=storage,
