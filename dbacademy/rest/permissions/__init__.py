c0ff3c3e75dfcdb5a99b6bccf18f3afcaa4e6976
Jacob D. Parr <jacob.parr@databricks.com>
2022-07-28 10:48:38 -0500

    Added permissions for cluster_policies

--- dbacademy/rest/permissions/__init__.py
+++ dbacademy/rest/permissions/__init__.py
@@ -6,9 +6,15 @@ __all__ = ["Permissions"]
 class Permissions(ApiContainer):
     def __init__(self, client: ApiClient):
         self.client = client
+
         from dbacademy.rest.permissions.jobs import Jobs
         self.jobs = Jobs(client)
+
         from dbacademy.rest.permissions.pools import Pools
         self.pools = Pools(client)
+
         from dbacademy.rest.permissions.sql import Sql
         self.sql = Sql(client)
+
+        from dbacademy.rest.permissions.cluster_policies import ClusterPolicies
+        self.cluster_policies = ClusterPolicies(client)
