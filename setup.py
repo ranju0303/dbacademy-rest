4c50742aaa62e3064429c64a04f728a136b8aa57
Jacob D. Parr <jacob.parr@databricks.com>
2022-08-05 19:55:59 -0500

    Refactoring

--- setup.py
+++ setup.py
@@ -10,10 +10,20 @@ reqs = [
     'dbacademy-gems@git+https://github.com/databricks-academy/dbacademy-gems',
 ]
 
+def find_dbacademy_packages():
+    packages = find_packages(where="src")
+    if "dbacademy" in packages:
+        del packages[packages.index("dbacademy")]
+    # print("-"*80)
+    # print(packages)
+    # print("-"*80)
+    return packages
+
+
 setuptools.setup(
     name="dbacademy-rest",
     version="0.1",
     install_requires=reqs,
     package_dir={"dbacademy": "src/dbacademy"},
-    packages=find_packages("src")
+    packages=find_dbacademy_packages()
 )
